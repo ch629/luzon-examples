70bd62023b0e736a34444c2cd545028c11ea771a
ch629 <CharlieHowes97@outlook.com>
2019-05-18 16:16:48 +0100

    Implemented a CLI system to work within Luzon with a basic calculator.

---
+++ src/main/kotlin/com/luzon/examples/cli_interface/CliInterface.kt
@@ -0,0 +1,70 @@
+package com.luzon.examples.cli_interface
+
+import com.luzon.Luzon
+import com.luzon.reflection_engine.annotations.LzMethod
+import com.luzon.runtime.Environment
+import com.luzon.runtime.LzObject
+import com.luzon.runtime.nullObject
+import com.luzon.runtime.primitiveObject
+import java.util.*
+
+data class MenuItem(val name: String, val methodName: String)
+
+val menuItems = mutableListOf<MenuItem>()
+val scanner = Scanner(System.`in`)
+
+object Methods {
+    // Item Name, Method Name
+    @LzMethod(args = ["String", "String"])
+    fun addItem(args: List<Any>): LzObject {
+        menuItems.add(MenuItem(args[0] as String, args[1] as String))
+
+        return nullObject
+    }
+
+    @LzMethod(args = ["Any"])
+    fun println(args: List<Any>): LzObject {
+        println(args[0])
+
+        return nullObject
+    }
+
+    @LzMethod(name = "println")
+    fun printlnEmpty(args: List<Any>): LzObject {
+        println()
+
+        return nullObject
+    }
+
+    @LzMethod(args = ["Any"])
+    fun print(args: List<Any>): LzObject {
+        print(args[0])
+
+        return nullObject
+    }
+
+    @LzMethod
+    fun readLine(args: List<Any>) = primitiveObject(scanner.nextInt())
+}
+
+fun main() {
+    Luzon.runFile("src\\main\\resources\\CLI.lz")
+    Luzon.registerMethods(Methods::class)
+    val cliClass = Environment.global.invokeFunction("CLI", emptyList())
+    cliClass.invokeFunction("init", emptyList())
+
+    do {
+        println("Menu")
+        println("0 - Exit Program")
+
+        menuItems.forEachIndexed { i, item -> println("${i + 1} - ${item.name}") }
+
+        println()
+        print("Enter Option: ")
+        val line = scanner.nextInt() - 1
+
+        if (line >= 0 && line < menuItems.size) {
+            cliClass.invokeFunction(menuItems[line].methodName, emptyList())
+        }
+    } while (line >= 0)
+}
